fd0dd0eeb7a20a55acf61c37d3e22caaa3b58476
Andreas Leimbach <andreaslorozco@gmail.com>
2022-09-22 18:43:01 -0600

    feat: add database persistence for sessions
    * add database persistence for sessions

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,24 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
+const prisma = new PrismaClient()
+
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async session({ session, user, token }) {
+      const newSession = { ...session, user: { id: user.id, ...session.user }, ...token }
+      return newSession
+    }
+  }
 }
 
 export default NextAuth(authOptions)
