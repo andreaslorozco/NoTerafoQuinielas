097a98415a3d8ca8e00721d8976924b93f0f17df
Andreas Leimbach <andreaslorozco@gmail.com>
2022-09-30 15:52:40 -0600

    feat: dont render my-lobbies page if no session (#7)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,9 @@ const Navbar = () => {
         />
         <MenuList>
           <CustomMenuItem to="/">Home</CustomMenuItem>
-          <CustomMenuItem to="/my-lobbies">My Lobbies</CustomMenuItem>
+          {session && (
+            <CustomMenuItem to="/my-lobbies">My Lobbies</CustomMenuItem>
+          )}
           {!session && (
             <CustomMenuItem onClick={() => signIn()}>Log in</CustomMenuItem>
           )}
