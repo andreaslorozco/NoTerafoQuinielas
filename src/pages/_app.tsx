d1343d657c89fc54aab4ac2ea407cde8e57c0bec
Andreas Leimbach <andreaslorozco@gmail.com>
2022-09-30 17:08:34 -0600

    improve visuals; scaffold new pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,23 @@
 import { ChakraProvider, Container } from "@chakra-ui/react"
 import { Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
-import type { AppProps } from "next/app"
+import Head from "next/head"
+import Navbar from "../components/Navbar"
 import theme from "../theme"
+
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <ChakraProvider theme={theme}>
       <SessionProvider session={pageProps.session}>
-        <Container maxW="container.md" mt={"2em"}>
+        <Head>
+          <title>NoterafoQuinielas</title>
+          <meta name="description" content="NoTerafoQuinielas" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+        <Container boxSize="lg" mt={"2em"}>
           <Component {...pageProps} />
         </Container>
       </SessionProvider>
