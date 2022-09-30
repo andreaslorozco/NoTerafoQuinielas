171b1dac12015286fca0992d9346b963d6e95056
Andreas Leimbach <andreaslorozco@gmail.com>
2022-09-30 15:49:23 -0600

    dont render my-lobbies page if no session

--- src/pages/my-lobbies.tsx
+++ src/pages/my-lobbies.tsx
@@ -1,7 +1,14 @@
+import { unstable_getServerSession } from "next-auth/next"
+import { authOptions } from "./api/auth/[...nextauth]"
 import { Alert, AlertIcon, Box } from "@chakra-ui/react"
 import Navbar from "../components/Navbar"
 
+import { GetServerSidePropsContext } from "next/types"
+// import { Session } from "next-auth"
+
 const MyLobbies = () => {
+  // todo: use session to fetch user lobbies?
+  // { session }: { session: Session }
   return (
     <>
       <Navbar />
@@ -16,3 +23,25 @@ const MyLobbies = () => {
 }
 
 export default MyLobbies
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  )
+  // redirect to the homepage if no session
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: {
+      session,
+    },
+  }
+}
