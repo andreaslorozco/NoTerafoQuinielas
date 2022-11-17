5f124f50b985a44208c618ad1369c1952c044b22
Andreas Leimbach <andreaslorozco@gmail.com>
2022-11-16 20:12:23 -0500

    update predictionExists state when prediction is saved

--- src/components/PredictionForm.tsx
+++ src/components/PredictionForm.tsx
@@ -58,6 +58,7 @@ const PredictionForm = ({ game, userId }: Props) => {
     })
     await response.json()
     setSubmitting(false)
+    setPredictionExists(true)
     toast({
       title: "Prediction saved",
       status: "success",
