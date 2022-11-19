31c3f9b89f1b3ed53ca50d34ff01a17f117c2262
Andreas Leimbach <andreaslorozco@gmail.com>
2022-11-19 17:44:35 -0500

    feat: improve home page ux
    display spinner after a button on home page is clicked

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { Box, Button, Heading } from "@chakra-ui/react"
+import { Box, Button, Flex, Heading, Spinner } from "@chakra-ui/react"
 import type { NextPage } from "next"
-import { useSession, signIn, signOut } from "next-auth/react"
-import NextLink from "next/link"
+import { useSession, signIn } from "next-auth/react"
+
 import Head from "next/head"
+import { useState } from "react"
+import HomeMenuButtons from "../components/HomeMenuButtons"
 
 const Home: NextPage = () => {
   const { data: session } = useSession()
+  const [loading, setLoading] = useState(false)
   return (
     <>
       <Box as="main">
@@ -17,31 +20,17 @@ const Home: NextPage = () => {
             <Heading size="lg">
               Welcome, {session.user?.name.split(" ")[0]}!
             </Heading>
-            <Box mt={"2em"}>
-              <NextLink href="/my-lobbies" passHref>
-                <Button colorScheme="blue" w="100%">
-                  Go to My Lobbies
-                </Button>
-              </NextLink>
-              <NextLink href="/new-lobby" passHref>
-                <Button colorScheme="messenger" w="100%" mt={"1em"}>
-                  Create a Lobby
-                </Button>
-              </NextLink>
-              <NextLink href="/my-lobbies" passHref>
-                <Button colorScheme="cyan" w="100%" mt={"1em"}>
-                  Join a Lobby
-                </Button>
-              </NextLink>
-              <Button
-                colorScheme="red"
-                w="100%"
-                mt={"1em"}
-                onClick={() => signOut()}
+            {loading && (
+              <Flex
+                height="208px"
+                alignItems={"center"}
+                justifyContent={"center"}
+                mt={"2em"}
               >
-                Logout
-              </Button>
-            </Box>
+                <Spinner size="xl" />
+              </Flex>
+            )}
+            {!loading && <HomeMenuButtons setLoading={setLoading} />}
           </>
         )}
         {!session && (
