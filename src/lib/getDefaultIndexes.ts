57372be7b774ac2b7114d602b63a2145c3ccb8d2
andreaslorozco <andreaslorozco@gmail.com>
2022-11-25 14:09:08 -0500

    hide completed games on predictions page

--- src/lib/getDefaultIndexes.ts
+++ src/lib/getDefaultIndexes.ts
@@ -2,10 +2,8 @@ import { GameWithTeams } from "../types"
 
 export const getDefaultIndexes = (groupedGames: GameWithTeams[][]) => {
   return groupedGames.reduce((indexes: number[], group, index) => {
-    const groupDate = new Date(group[0].date)
-    const now = new Date(Date.now())
-
-    if (groupDate > now) indexes.push(index)
+    if (group.every((g) => g.completed)) return indexes
+    indexes.push(index)
     return indexes
   }, [])
 }
